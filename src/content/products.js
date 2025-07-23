bc0d856ca8c581ffd18b6946bb798f7a8d36efde
ctrujillo12 <cheyenneettrujillo@gmail.com>
2025-07-22 19:43:58 -0700

    fixed shop carousel

---
+++ src/content/products.js
@@ -0,0 +1,77 @@
+export const products = [
+  {
+    name: "Velvet Blazer",
+    image: "https://images.unsplash.com/photo-1551232864-3f95fbb6f6c5",
+    link: "https://example.com/product1",
+  },
+  {
+    name: "Chunky Loafers",
+    image: "https://images.unsplash.com/photo-1600180758890-6c8de8c1f3fc",
+    link: "https://example.com/product2",
+  },
+  {
+    name: "Satin Slip Dress",
+    image: "https://images.unsplash.com/photo-1542062703-1f1d16fa2b6c",
+    link: "https://example.com/product3",
+  },
+  {
+    name: "Oversized Sunglasses",
+    image: "https://images.unsplash.com/photo-1600180758581-0b850dba740d",
+    link: "https://example.com/product4",
+  },
+  {
+    name: "Tweed Mini Skirt",
+    image: "https://images.unsplash.com/photo-1587614382346-4ec7c07799f4",
+    link: "https://example.com/product5",
+  },
+  {
+    name: "Leather Crossbody Bag",
+    image: "https://images.unsplash.com/photo-1605296867304-46d5465a13f1",
+    link: "https://example.com/product6",
+  },
+  {
+    name: "Platform Boots",
+    image: "https://images.unsplash.com/photo-1612250486701-3f2b1a6c1d90",
+    link: "https://example.com/product7",
+  },
+  {
+    name: "Faux Fur Coat",
+    image: "https://images.unsplash.com/photo-1556909190-447bbed5276e",
+    link: "https://example.com/product8",
+  },
+  {
+    name: "Wide-Leg Trousers",
+    image: "https://images.unsplash.com/photo-1588515028451-c1d9a0e9eb09",
+    link: "https://example.com/product9",
+  },
+  {
+    name: "Gold Statement Earrings",
+    image: "https://images.unsplash.com/photo-1623091412210-58c24f52dc5c",
+    link: "https://example.com/product10",
+  },
+  {
+    name: "Structured Blouse",
+    image: "https://images.unsplash.com/photo-1584043720375-bd5dc3f5771f",
+    link: "https://example.com/product11",
+  },
+  {
+    name: "Silk Headscarf",
+    image: "https://images.unsplash.com/photo-1592878892232-0a6c317fbbe3",
+    link: "https://example.com/product12",
+  },
+  {
+    name: "Denim Corset Top",
+    image: "https://images.unsplash.com/photo-1618354691229-6fd263b03b7f",
+    link: "https://example.com/product13",
+  },
+  {
+    name: "Lace-Up Heels",
+    image: "https://images.unsplash.com/photo-1618354691086-59c56c5c3e11",
+    link: "https://example.com/product14",
+  },
+  {
+    name: "Crochet Handbag",
+    image: "https://images.unsplash.com/photo-1618354690905-b24cf8dd4f33",
+    link: "https://example.com/product15",
+  },
+];
