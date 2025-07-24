38115b6af40f96c355717e1f121afbf41bae3f89
ctrujillo12 <cheyenneettrujillo@gmail.com>
2025-07-23 21:28:38 -0700

    fixed shopcarousel and new layout

--- src/content/products.js
+++ src/content/products.js
@@ -1,77 +1,92 @@
 export const products = [
   {
-    name: "Velvet Blazer",
-    image: "https://images.unsplash.com/photo-1551232864-3f95fbb6f6c5",
-    link: "https://example.com/product1",
+    name: "Nanette Lepore halter",
+    image: '/images/halter.jpg', // Make sure this image exists in your /public/images folder
+    link: "https://poshmark.com/listing/Nanette-Lepore-Silk-Sleeveless-Floral-68470493310f1c933d8a8a47",
+    price: "$29"
   },
   {
-    name: "Chunky Loafers",
-    image: "https://images.unsplash.com/photo-1600180758890-6c8de8c1f3fc",
+    name: "Sneakers",
+    image: "/images/fredperry.jpg",
     link: "https://example.com/product2",
+    price: "$120"
   },
   {
-    name: "Satin Slip Dress",
-    image: "https://images.unsplash.com/photo-1542062703-1f1d16fa2b6c",
+    name: "Marc Jacobs cardigan",
+    image: "/images/mjcardigan.png",
     link: "https://example.com/product3",
+    price: "$36"
   },
   {
-    name: "Oversized Sunglasses",
-    image: "https://images.unsplash.com/photo-1600180758581-0b850dba740d",
+    name: "Ombre skirt",
+    image: "/images/topshopskirt.png",
     link: "https://example.com/product4",
+    price: "$25"
   },
   {
-    name: "Tweed Mini Skirt",
-    image: "https://images.unsplash.com/photo-1587614382346-4ec7c07799f4",
+    name: "Bebe ruffle top",
+    image: "/images/beberuffle.png",
     link: "https://example.com/product5",
+    price: "$50"
   },
   {
-    name: "Leather Crossbody Bag",
-    image: "https://images.unsplash.com/photo-1605296867304-46d5465a13f1",
+    name: "Balenciaga dress",
+    image: "/images/balencidress.png",
     link: "https://example.com/product6",
+    price: "$250"
   },
   {
-    name: "Platform Boots",
-    image: "https://images.unsplash.com/photo-1612250486701-3f2b1a6c1d90",
+    name: "Guess top",
+    image: "/images/guesstop.png",
     link: "https://example.com/product7",
+    price: "$50"
   },
   {
     name: "Faux Fur Coat",
     image: "https://images.unsplash.com/photo-1556909190-447bbed5276e",
     link: "https://example.com/product8",
+    price: "$210"
   },
   {
     name: "Wide-Leg Trousers",
     image: "https://images.unsplash.com/photo-1588515028451-c1d9a0e9eb09",
     link: "https://example.com/product9",
+    price: "$80"
   },
   {
     name: "Gold Statement Earrings",
     image: "https://images.unsplash.com/photo-1623091412210-58c24f52dc5c",
     link: "https://example.com/product10",
+    price: "$55"
   },
   {
     name: "Structured Blouse",
     image: "https://images.unsplash.com/photo-1584043720375-bd5dc3f5771f",
     link: "https://example.com/product11",
+    price: "$70"
   },
   {
     name: "Silk Headscarf",
     image: "https://images.unsplash.com/photo-1592878892232-0a6c317fbbe3",
     link: "https://example.com/product12",
+    price: "$35"
   },
   {
     name: "Denim Corset Top",
     image: "https://images.unsplash.com/photo-1618354691229-6fd263b03b7f",
     link: "https://example.com/product13",
+    price: "$60"
   },
   {
     name: "Lace-Up Heels",
     image: "https://images.unsplash.com/photo-1618354691086-59c56c5c3e11",
     link: "https://example.com/product14",
+    price: "$90"
   },
   {
     name: "Crochet Handbag",
     image: "https://images.unsplash.com/photo-1618354690905-b24cf8dd4f33",
     link: "https://example.com/product15",
+    price: "$85"
   },
 ];
