f0c940abbbadf8de02a435fff7fd7d50160b06cb
ctrujillo12 <cheyenneettrujillo@gmail.com>
2025-08-13 21:42:07 -0700

    leave out spotify backend

--- dist/chunks/spotifyTokenStore_BHbDpUA0.mjs
+++
@@ -1,9 +0,0 @@
-let access_token = null;
-function setToken(token) {
-  access_token = token;
-}
-function getToken() {
-  return access_token;
-}
-
-export { getToken as g, setToken as s };
--- src/lib/spotifyTokenStore.ts
+++ src/lib/spotifyTokenStore.ts
@@ -21,11 +21,11 @@
 //  */
 // export function clearToken() {
 //   accessToken = null;
-// }
-export const clientId = 'f6af48791a99465e8a5eb950464a9c15';
+// // }
+// export const clientId = 'f6af48791a99465e8a5eb950464a9c15';
 
-export const redirectUri = 'http://127.0.0.1:4321/api/spotify/callback';
+// export const redirectUri = 'http://127.0.0.1:4321/api/spotify/callback';
 
-export const scopes = encodeURIComponent('user-read-private user-read-email user-top-read');
+// export const scopes = encodeURIComponent('user-read-private user-read-email user-top-read');
 
-export const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scopes}`;
+// export const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scopes}`;
