754ccaa0bd7d146b1990259c473053714375cc20
ctrujillo12 <cheyenneettrujillo@gmail.com>
2025-08-16 15:23:14 -0700

    night mode

---
+++ src/content/edgy.js
@@ -0,0 +1,62 @@
+export const productsEdgy = [
+  {
+    name: "Agent provocateur wim top",
+    image: "/images/ap_swim_top.png",
+    link: "https://www.agentprovocateur.com/us_en/ap12286001674-laticia-bikini-top-in-black-neon-pink-32429",
+    price: "$60"
+  },
+  {
+    name: "Red Valentino mini skirt",
+    image: "/images/rv_skirt.webp",
+    link: "https://www.therealreal.com/products/women/clothing/skirts/red-valentino-printed-mini-skirt-px25s",
+    price: "$38"
+  },
+  {
+    name: "CDG top",
+    image: "/images/cdg_longsleeve.webp",
+    link: "https://www.therealreal.com/products/women/clothing/tops/comme-des-garcons-comme-des-garcons-scoop-neck-long-sleeve-top-qxc1f",
+    price: "$56"
+  },
+  {
+    name: "Prada skirt",
+    image: "/images/prada_skirt.webp",
+    link: "https://www.ebay.com/itm/143292552414",
+    price: "$175"
+  },
+  {
+    name: "SS925 Pendant",
+    image: "/images/gothic_pendant.webp",
+    link: "https://www.ebay.com/itm/133115173640",
+    price: "$31"
+  },
+  {
+    name: "Junya CDG top",
+    image: "/images/jwcdg_neon_top.webp",
+    link: "https://www.therealreal.com/products/women/clothing/tops/junya-watanabe-comme-des-garcons-vintage-2002-t-shirt-qtsy9",
+    price: "$180"
+  },
+  {
+    name: "Helmut lang top",
+    image: "/images/hl_longsleeve.avif",
+    link: "https://www.mercari.com/us/item/m70088295442/?ref=search_results",
+    price: "$55"
+  },
+  {
+    name: "GOA Paris sweater",
+    image: "/images/goa_sweater.jpg",
+    link: "https://poshmark.com/listing/GOA-Paris-Embroidered-Sweater-605bc6dc67bd91f4bb7cd2bc",
+    price: "$91"
+  },
+  {
+    name: "Molly Goddard cardigan",
+    image: "/images/mollygoddard_cardigan.webp",
+    link: "https://poshmark.com/listing/Molly-Goddard-cardigan-sweater-66d49f8dfe7e2cf2d14f17c4",
+    price: "$180"
+  },
+  {
+    name: "Jil Sander sweater",
+    image: "/images/jilsander_shortsleeve_sweater.webp",
+    link: "https://www.therealreal.com/products/women/clothing/knitwear/jil-sander-cashmere-crew-neck-sweater-qd7o1",
+    price: "$75"
+  },
+];
