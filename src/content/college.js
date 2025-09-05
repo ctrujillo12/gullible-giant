a5550fabd4652dfda853822d298e636cc78c1ccd
ctrujillo12 <cheyenneettrujillo@gmail.com>
2025-09-04 23:03:18 -0700

    school carousel

---
+++ src/content/college.js
@@ -0,0 +1,45 @@
+export const productsCollege = [
+  {
+    name: "Chanel Boots",
+    image: "/images/chanel_boots.webp",
+    link: "https://www.therealreal.com/products/jewelry/necklaces/collar/pamela-love-spike-collar-necklace-qjejn",
+    price: "$100"
+  },
+  {
+    name: "Brandy skirt",
+    image: "/images/brandy_skirt.webp",
+    link: "https://poshmark.com/listing/DRIES-VAN-NOTEN-SNEAKER-WEDGE-SIZE-365-65f8b356c5df6c7fa8e37b4f",
+    price: "$32"
+  },
+  {
+    name: "Kotn track pants",
+    image: "/images/kotn_sweats.webp",
+    link: "https://www.therealreal.com/products/jewelry/rings/cocktail-ring/kate-spade-new-york-arctic-friends-polar-bear-pave-ring-qr3nf",
+    price: "$148"
+  },
+  {
+    name: "JPG top",
+    image: "/images/jpg_cowl.webp",
+    link: "https://poshmark.com/listing/MCM-mini-Boston-bag-6606d91a92e491386801376a",
+    price: "$63"
+  },
+  {
+    name: "Isabel Marant sneakers",
+    image: "/images/marant_sneakers.webp",
+    link: "https://www.therealreal.com/products/jewelry/rings/cocktail-ring/kate-spade-new-york-arctic-friends-polar-bear-pave-ring-qr3nf",
+    price: "$100"
+  },
+  {
+    name: "Sneakers",
+    image: "/images/fredperry.jpg",
+    link: "https://www.therealreal.com/products/men/shoes/sneakers/fred-perry-x-comme-des-garcons-printed-embroidered-accent-sneakers-qcior",
+    price: "$120"
+  },
+  {
+    name: "Marc Jacobs",
+    image: "/images/mj_sunglasses.png",
+    link: "https://www.therealreal.com/products/women/accessories/sunglasses/marc-jacobs-wayfarer-tinted-sunglasses-qrqyi",
+    price: "$56"
+  },
+  
+];
